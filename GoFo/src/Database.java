3f28beb4575057100cdf59f96450e3d24a1ef962
Hussein Okasha <husseinokasha13@gmail.com>
2020-05-21 17:39:49 +0200

    Adding setters for player + basic structure of class Database.

--- GoFo/src/Database.java
+++ GoFo/src/Database.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Database {
+    // attributes....
+    private static  ArrayList<Player> players;
+    private static  ArrayList<Playground>playgrounds;
+    private static  ArrayList<PlaygroundOwner>playgroundOwners;
+    private static  ArrayList<Administrator>administrators;
+    // methods...
+    public Database (){
+        players= new ArrayList<Player>();
+        playgrounds= new ArrayList<Playground>();
+        playgroundOwners = new ArrayList<PlaygroundOwner>();
+        administrators = new ArrayList<Administrator>();
+    }
+    void AddPlayer(Player player){
+
+    }
+    void AddPlaygroundOwner(PlaygroundOwner playgroundOwner){
+
+    }
+    void addPlayground(Playground playground){
+
+    }
+    void addAdministrator(Administrator administrator){
+
+    }
+    // add functions to remove also ....
+
+
 }
--- GoFo/src/Player.java
+++ GoFo/src/Player.java
@@ -1,2 +1,62 @@
+import java.util.ArrayList;
+
 public class Player {
+    // Attributes....
+    private String name;
+    private int ID;
+    private String Password;
+    private String email;
+    private ArrayList<String> notifications ;
+    private Team team ;
+    private ArrayList<Booking> bookings;
+    private double balance;
+    private Database database; // new attribute.....
+    //methods..
+    public Player (String name , int ID , String password , String email , ArrayList<String>notifications , Team team , ArrayList<Booking> bookings, double balance ){
+            this.name=name;
+            this.ID=ID;
+            this.Password= password;
+            this.email=email;
+            this.notifications=notifications;
+            this.team=team;
+            this.bookings=bookings;
+            this.balance= balance;
+            database.AddPlayer(this);
+
+    }
+    //setters...
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void addNotification(String notification){
+        if (notification!=""){
+            this.notifications.add(notification);
+        }
+    }
+    public void createTeam(){
+       //  this.team.addPlayer();
+    }
+    public void bookPlayground (){
+
+    }
+
+
+
+}
+class Main{
+    public static void main(String args[]){
+        Player player = new Player("Hussein Okasha" , 123 , "kk%lll" , "husseinokasha13@gmail.com",null, null,null,2000);
+    }
 }
