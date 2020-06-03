5ff61cc2a98db2547732f4ca8fe15f621bd32892
Hussein Okasha <husseinokasha13@gmail.com>
2020-06-03 04:05:13 +0200

    edits on print statments

--- GoFo/src/Login.java
+++ GoFo/src/Login.java
@@ -1,33 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Login {
 
 
     // methods
-    boolean loginAsPlayer(Scanner scanner) {
+    boolean loginAsPlayer() {
         System.out.print("====================================\n");
-        System.out.println("login:");
-        System.out.print("enter the email: " );
-        //Scanner scanner = new Scanner(System.in);
-        String email = scanner.next() ;
+        System.out.println("Login:");
+        System.out.print("Enter the email: " );
+        boolean found=false;
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String email = reader.readLine();
+            System.out.print("Enter the password: ");
+            String password = reader.readLine();
+            for (int i = 0; i< Database.players.size(); i++) {
+                if (Database.players.get(i).getEmail().equalsIgnoreCase(email) && Database.players.get(i).getPassword().equals(password) ){
+                    Database.players.get(i).mainMenu();
+                    found=true;
+                }
 
-        System.out.print("enter the password");
-        String password = scanner.next();
-        for (int i = 0; i< Database.players.size(); i++) {
-            if (Database.players.get(i).getEmail().equalsIgnoreCase(email) && Database.players.get(i).getPassword().equals(password) ){
-                  Database.players.get(i).mainMenu();
             }
-
+            if(found==false){
+                System.out.println("Invalid username or password.");
+            }
         }
-        System.out.println("there isnot an account with this info pls make a registeration");
-        scanner.close();
+        catch (IOException e){
+             System.out.println("Invalid input");
+             loginAsPlayer();
+        }
+
+
+
         return false;
     }
     boolean loginAsPlaygroundOwner() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the email");
+        System.out.println("Enter the email: ");
         String email = scanner.next();
-        System.out.println("enter the password");
+        System.out.println("Enter the password: ");
         String password = scanner.next();
         for (int i = 0; i< Database.playgroundOwners.size(); i++) {
             if (Database.playgroundOwners.get(i).getEmail().equalsIgnoreCase(email) && Database.playgroundOwners.get(i).getPassword().equals(password)){
@@ -35,7 +49,7 @@ public class Login {
             }
 
         }
-        System.out.println("there isnot an account with this info pls make a registeration");
+        System.out.println("there isn`t an account with this info please register.");
         return false;
     }
     boolean loginAsAdminstartor() {
@@ -61,7 +75,7 @@ public class Login {
             //loginAsPlayer();
         }
         else if(role==2){
-            loginAsPlaygroundOwner();
+            //loginAsPlaygroundOwner();
         }
         else if(role==3){
             loginAsAdminstartor();
