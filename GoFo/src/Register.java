fba8210898619254b7a8e59d36ec6152c49f8c88
Hussein Okasha <husseinokasha13@gmail.com>
2020-06-03 02:18:16 +0200

    removing the scanners and use bufferedInputStream instead of it.

--- GoFo/src/Register.java
+++ GoFo/src/Register.java
@@ -1,142 +1,171 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Register {
 
 
     // methods...
     void registerAsPlayer(){
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
         Player player= new Player();
-        System.out.println("enter the name");
-        String name = scanner.next();
-        if(isCompleteString(name)==true){
-            player.setName(name);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlayer();
-        }
-        System.out.print("enter the ID");
-        int ID = scanner.nextInt();
-        if(isCompleteINT(ID)==true){
-            player.setID(ID);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlayer();
-        }
-        System.out.print("enter the password");
-        String password = scanner.next();
-        if(isCompleteString(name)==true ) {
-            player.setPassword(password);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlayer();
-        }
-        System.out.print("enter the e-mail");
-        String email = scanner.next();
-        if(isCompleteString(name)==true){
-            player.setEmail(email);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlayer();
-        }
-        Database.players.add(player);
+        System.out.print("enter the name:");
+        String name = null;
+        try {
+            name = reader.readLine();
+            if(isCompleteString(name)==true){
+                player.setName(name);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlayer();
+            }
+            System.out.print("enter the ID: ");
+            int ID = reader.read();
+            reader.readLine();
 
-        Login login = new Login();
-        login.loginAsPlayer(scanner);
-        scanner.close();
+            if(isCompleteINT(ID)==true){
+                player.setID(ID);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlayer();
+            }
+            System.out.print("enter the password:");
+            String password = reader.readLine();
+            if(isCompleteString(name)==true ) {
+                player.setPassword(password);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlayer();
+            }
+            System.out.print("enter the e-mail;");
+            String email = reader.readLine();
+            if(isCompleteString(name)==true){
+                player.setEmail(email);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlayer();
+            }
+            Database.players.add(player);
+
+            Login login = new Login();
+            login.loginAsPlayer();
+
+        }
+        catch (IOException e){
+            System.out.println("Invalid Input...");
+            registerAsPlayer();
+        }
+
+
     }
     void registerAsPlaygroundOwner(){
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
         PlaygroundOwner owner = new PlaygroundOwner() ;
-        System.out.println("enter the name");
-        String name = scanner.nextLine();
-        if(isCompleteString(name)==true){
-            owner.setName(name);
+        System.out.print("Enter the name: ");
+        try{
+            String name = reader.readLine();
+            if(isCompleteString(name)){
+                owner.setName(name);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlaygroundOwner();
+            }
+            System.out.print("Enter the ID: ");
+            int ID =  reader.read();
+            reader.readLine(); // without it , it will take the newline character as input for the next time you try to read
+            if(isCompleteINT(ID)){
+                owner.setID(ID);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlaygroundOwner();
+            }
+            System.out.print("Enter the password: ");
+            String password =  reader.readLine();
+            if(isCompleteString(name)) {
+                owner.setPassword(password);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlaygroundOwner();
+            }
+            System.out.print("Enter the e-mail: ");
+            String email =  reader.readLine();
+            if(isCompleteString(name)){
+                owner.setEmail(email);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsPlaygroundOwner();
+            }
+            Database.playgroundOwners.add(owner);
+
+            Login login = new Login();
+            login.loginAsPlaygroundOwner();
         }
-        else{
-            System.out.println("wrong input");
+        catch (IOException e){
+            System.out.println("Invalid input");
+            System.out.println("===============================================");
             registerAsPlaygroundOwner();
         }
-        System.out.println("enter the ID");
-        int ID =  scanner.nextInt();
-        if(isCompleteINT(ID)==true){
-            owner.setID(ID);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlaygroundOwner();
-        }
-        System.out.println("enter the password");
-        String password =  scanner.nextLine();
-        if(isCompleteString(name)==true ) {
-            owner.setPassword(password);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlaygroundOwner();
-        }
-        System.out.println("enter the e-mail");
-        String email =  scanner.nextLine();
-        if(isCompleteString(name)==true){
-            owner.setEmail(email);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsPlaygroundOwner();
-        }
-        Database.playgroundOwners.add(owner);
-        scanner.close();
-        Login login = new Login();
-        login.loginAsPlaygroundOwner();
     }
     void registerAsAdministrator(){
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
         Administrator admin = new Administrator();
         System.out.println("enter the name");
-        String name = scanner.nextLine();
-        if(isCompleteString(name)==true){
-            admin.setName(name);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsAdministrator();
-        }
-        System.out.println("enter the ID");
-        int ID = scanner.nextInt();
-        scanner.nextLine();
-        if(isCompleteINT(ID)==true){
-            admin.setID(ID);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsAdministrator();
-        }
-        System.out.println("enter the password");
-        String password = scanner.next();
-        if(isCompleteString(name)==true ) {
-            admin.setPassword(password);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsAdministrator();
-        }
-        System.out.println("enter the e-mail");
-        String email = scanner.next();
-        if(isCompleteString(name)==true){
-            admin.setEmail(email);
-        }
-        else{
-            System.out.println("wrong input");
-            registerAsAdministrator();
-        }
-        Database.administrators.add(admin);
+        try {
+            String name = reader.readLine();
+            if(isCompleteString(name)==true){
+                admin.setName(name);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsAdministrator();
+            }
+            System.out.println("enter the ID");
+            int ID = reader.read();
+            reader.readLine();
+            if(isCompleteINT(ID)==true){
+                admin.setID(ID);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsAdministrator();
+            }
+            System.out.println("enter the password");
+            String password = reader.readLine();
+            if(isCompleteString(password)) {
+                admin.setPassword(password);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsAdministrator();
+            }
+            System.out.println("enter the e-mail");
+            String email = reader.readLine();
+            if(isCompleteString(name)){
+                admin.setEmail(email);
+            }
+            else{
+                System.out.println("wrong input");
+                registerAsAdministrator();
+            }
+            Database.administrators.add(admin);
 
-        Login login = new Login();
-        login.loginAsAdminstartor();
-        scanner.close();
+            Login login = new Login();
+            login.loginAsAdminstartor();
+
+        }
+        catch (IOException e){
+            System.out.println("Invalid input");
+            System.out.println("=======================================");
+            registerAsAdministrator();
+        }
+
     }
     boolean isCompleteString(String input){
         if(input.equalsIgnoreCase("")){return false ;}
@@ -147,26 +176,31 @@ public class Register {
         else {return true;}
     }
     void chooseRole(){
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
         System.out.print("1-As player\n");
         System.out.print("2-As playgroundOwner\n");
         System.out.print("3-As Administrator\n");
         System.out.print("enter your role: ");
-        String role = scanner.next();
-        if(role.equalsIgnoreCase("1")){
-            registerAsPlayer();
+        try{
+            String role = reader.readLine();
+            if(role.equalsIgnoreCase("1")){
+                registerAsPlayer();
+            }
+            else if(role.equalsIgnoreCase("2")){
+                registerAsPlaygroundOwner();
+            }
+            else if(role.equalsIgnoreCase("3")){
+                registerAsAdministrator();
+            }
+            else{
+                System.out.println("invalid choice");
+                chooseRole();
+            }
         }
-        else if(role.equalsIgnoreCase("1")){
-            registerAsPlaygroundOwner();
-        }
-        else if(role.equalsIgnoreCase("1")){
-            registerAsAdministrator();
-        }
-        else{
-            System.out.println("invalid choice");
+        catch (IOException e){
+            System.out.println("invalid Input");
             chooseRole();
         }
-        //scanner.close();
     }
 
 }
