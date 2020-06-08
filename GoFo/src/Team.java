8e39586ef474f021390dcc5391e199d6a90e7c3c
osamaalrowhani <osamaalrowhani@gmail.com>
2020-06-08 22:15:08 +0200

    create team constructor

--- GoFo/src/Team.java
+++ GoFo/src/Team.java
@@ -1,2 +1,34 @@
-public class  Team {
+
+import java.util.ArrayList;
+
+public class Team {
+	
+	//attributtes
+	private arrayList<player> players;
+	private Player teamCreator;
+	
+	//constractors
+	public Team() {
+		this.players=0;
+		this.teamCreator = 0 ;
+	}
+	
+	public Team(arrayList<player> players , Player teamCreator) {
+		this.players = players;
+		this.teamCreator = teamCreator;
+	}
+	
+	//methods
+	public void addPlayer() {
+		
+	}
+	
+	public void removePlayer() {
+		
+	}
+	
+	public void sendInvitation(){
+		
+	}
+	
 }
