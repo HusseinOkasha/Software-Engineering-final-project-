a096f531a5db594e3c8e37fabee8d019068b06a0
osamaalrowhani <osamaalrowhani@gmail.com>
2020-06-10 00:19:57 +0200

    some edit

--- GoFo/src/Team.java
+++ GoFo/src/Team.java
@@ -4,29 +4,26 @@ import java.util.ArrayList;
 public class Team {
 	
 	//attributtes
-	private arrayList<Player> players;
+	private ArrayList<Player> players;
 	private Player teamCreator;
 	
 	//constractors
 	public Team() {
-		this.players= new arrayList<Player>();
+		this.players= new ArrayList<Player>();
 		this.teamCreator = null ;
 	}
 	
-	public Team(arrayList<player> players , Player teamCreator) {
+	public Team(ArrayList<player> players , Player teamCreator) {
 		this.players = players;
 		this.teamCreator = teamCreator;
 	}
 	
 	//methods
 	public void addPlayer() {
-		//player = new Player();
+		ArrayList<Player> player = new Player();
 		
 	}
 	
-	
-	
-	\\
 	public void removePlayer() {
 		
 	}
